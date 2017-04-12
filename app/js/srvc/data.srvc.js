f060d6fa2f6475df5426ea39e2446b8f6238a658
Mac Os <Jesus@MacBook-Pro-de-Mac.local>
2017-04-12 01:43:32 -0500

    cambios

--- app/js/srvc/data.srvc.js
+++ app/js/srvc/data.srvc.js
@@ -5,12 +5,12 @@
   .service('DataService', dataService);
   dataService.$inject = ['$http'];
   function dataService($http) {
-    var develoment = true;
+    var develoment = false;
     //urls
     var filtersUrl = {
-      discipline: develoment ? "/data/discipline.json" : "http://www.horizontemma.com/wp-json/wp/v2/disciplinas"
+      discipline: develoment ? "/data/discipline.json" : "http://www.horizontemma.com/?rest_route=/wp/v2/disciplinas"
     };
-    var instituteUrl = develoment ? "/data/institutes.json" : "http://www.horizontemma.com/wp-json/wp/v2/academias";
+    var instituteUrl = develoment ? "/data/institutes.json" : "http://www.horizontemma.com/?rest_route=/wp/v2/academias";
     var errorCallbackService = function (error) {
       console.log("Ha ocurrido un error en la petición");
       console.log(error);
