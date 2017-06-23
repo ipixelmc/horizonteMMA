96f6f11f3ec3eb9f04ba4973cb7d2c32b144a3c4
Mac Os <Jesus@MacBook-Pro-de-Mac.local>
2017-06-23 01:21:57 -0500

    responsive enhacement

--- app/js/ctrl/main.js
+++ app/js/ctrl/main.js
@@ -5,6 +5,19 @@
   .controller('MainController', mainController);
   mainController.$inject = [];
   function mainController() {
+
     var ctrl = this;
+
+    ctrl.page = {
+    	mapActive: false,
+    	filters: ''
     }
-  })();
+
+    ctrl.toggleView = function(){
+    	console.log('test');
+    	ctrl.page.mapActive = !ctrl.page.mapActive;
+    }
+
+  }
+
+})();
